91145587c9ec192a979cbdea0a18951ed52e5b1f
Bart Bilos <squantmuts+git@gmail.com>
2026-09-04 12:08:50 +0200

    WIP setting storage class

--- firmware/common/inc/settings_storage.hpp
+++ firmware/common/inc/settings_storage.hpp
@@ -11,6 +11,7 @@
 #define SETTINGS_STORAGE_HPP
 
 #include <cstdint>
+#include <libmcu/libmcu.hpp>
 
 namespace detail {
 /**
@@ -19,8 +20,8 @@ namespace detail {
  */
 template <typename Application_settings>
 struct Settings_storage_record {
+  uint8_t magic_version;          //! signature also used as version
   uint8_t sequence_number;        //! Sequence number
-  uint8_t version;                //! Version of settings structure, will ignore mismatches
   uint16_t checksum;              //! checksum, simple 8 bit add is used for now
   Application_settings settings;  //! Application settings
 };
@@ -31,26 +32,138 @@ struct Settings_storage_record {
  * @tparam Application_settings structure to use for settings storage
  * @todo pass EEPROM class to settings storage
  */
-template <typename Application_settings>
+template <typename Application_settings, auto &storage_driver>
 struct Settings_storage : public libmcu::NonBlocking {
   /**
    * @brief Construct and initialize a new settings_storage object
+   * @todo add buffer for writing to storage driver
    */
-  Settings_storage() {}
+  Settings_storage(std::span<std::uint8_t> storage_buffer, std::uint8_t magic_version)
+    : state{libmcu::States::Uninitialized},
+      storage_buffer{storage_buffer},
+      magic_version{magic_version},
+      current_address{storage_driver.size()},
+      search_address{0},
+      sequence_number{0} {}
+  /**
+   * @brief Get current state
+   * @return Current state
+   */
+  libmcu::States get_state() {
+    return state;
+  }
   /**
    * @brief Initialize the settings storage
-   * Goes through EEPROM and loads latest settings or defaults
-   * @todo parameters definition for settings store and defaults
+   * @param settings Settings to initialize
+   * @param defaults Default settings
    */
-  void init(Application_settings settings, Application_settings defaults) {}
+  void init(Application_settings &settings, Application_settings &defaults) {
+    settings_store = &settings;
+    defaults_store = &defaults;
+    state = libmcu::States::Initializing;
+    storage_driver.read(search_address, storage_buffer, this);
+  }
+  /**
+   * @brief Deinitialize the settings storage
+   */
+  void deinit() {
+    state = libmcu::States::Uninitialized;
+    current_address = storage_driver.size();
+    search_address = 0;
+    sequence_number = 0;
+  }
   /**
    * @brief Save the settings passed on to the storage
-   *
+   * @param settings Settings to save
    */
-  void save() {}
+  void save(Application_settings settings) {}
+  /**
+   * @brief Progress processing of any operation
+   */
+  void progress() final {
+    switch (state) {
+      case libmcu::States::Initializing:
+        break;
+
+      default:
+        break;
+    }
+  }
+  /**
+   * @brief Callback used by driver to notify completion
+   * @param result Callback result
+   */
+  void callback(libmcu::Results result) final {
+    (void)result;
+    switch (state) {
+      case libmcu::States::Initializing:
+        if (result == libmcu::Results::NoError) {
+          detail::Settings_storage_record<Application_settings> *record =
+            reinterpret_cast<detail::Settings_storage_record<Application_settings> *>(storage_buffer.data());
+
+          // analyze record
+          if (record->magic_version == magic_version) {
+            // check checksum
+            // check sequence number, check also for wrapping
+            // if all is fine, write to current address
+            current_address = search_address;
+          }
+
+          // go to next page
+          search_address += storage_driver.page_size();
+          if (search_address >= storage_driver.size()) {
+            // did we find something?
+            if (current_address != storage_driver.size()) {
+              // We did, read from recorded address
+              storage_driver.read(current_address, storage_buffer, this);
+              state = libmcu::States::busy_reading;
+            } else {
+              // We did not, write default settings at address 0
+              current_address = 0;
+              for (auto &element : storage_buffer) {
+                element = 0;
+              }
+              record->magic_version = magic_version;
+              record->sequence_number = 0;
+              record->checksum = 0;
+              record->settings = *defaults_store;
+              storage_driver.write(current_address, storage_buffer, this);
+              *settings_store = *defaults_store;
+              state = libmcu::States::busy_writing;
+            }
+          } else {
+            storage_driver.read(search_address, storage_buffer, this);
+          }
+        } else {
+          state = libmcu::States::ErrorFatal;
+        }
+        break;
+      case libmcu::States::busy_writing:
+        if (result == libmcu::Results::NoError) {
+          state = libmcu::States::Idle;
+        }
+        break;
+      case libmcu::States::busy_reading:
+        if (result == libmcu::Results::NoError) {
+          detail::Settings_storage_record<Application_settings> *record =
+            reinterpret_cast<detail::Settings_storage_record<Application_settings> *>(storage_buffer.data());
+          *settings_store = record->settings;
+          state = libmcu::States::Idle;
+        }
+      default:
+        break;
+    }
+  }
 
  private:
-  //! @todo some kind of state variable if it is busy? Yes, is a async class
+  libmcu::States state;
+  Application_settings *settings_store, *defaults_store;
+  std::span<std::uint8_t> storage_buffer;
+  std::uint8_t magic_version;
+  std::size_t current_address;
+  std::size_t search_address;
+  std::uint8_t sequence_number;
+  //! @todo Stride for EEPROM?
 };
 
 #endif
--- firmware/projects/unittests/src/settings_storage.cpp
+++ firmware/projects/unittests/src/settings_storage.cpp
@@ -7,6 +7,152 @@
  * @file settings_storage.cpp
  * @brief Unitttests for settings storage class
  */
+#include <cstdint>
+#include <array>
+#include <span>
+#include <algorithm>
 #include <minunit.h>
 #include <pc/libmcu_PC_hal.hpp>
 #include "settings_storage.hpp"
+
+/**
+ * @brief Settings storage test structure
+ */
+struct Test_storage {
+  std::uint8_t value_one;
+  std::uint16_t value_two;
+  std::uint32_t value_three;
+};
+/**
+ * @brief Storage driver mock
+ */
+struct Storage_driver_mock {
+  Storage_driver_mock(std::span<std::uint8_t> storage, std::size_t page_size)
+    : storage{storage}, storage_page_size{page_size}, storage_size{storage.size()}, last_call_callback{nullptr} {}
+  void read(std::size_t address, std::span<std::uint8_t> buffer, libmcu::NonBlocking* callback = nullptr) {
+    if (address > storage.size()) {
+      minunit_report("Storage_driver_mock: Read out of bounds\n");
+      return;
+    }
+    for (std::size_t index = 0; index < buffer.size(); index++) {
+      buffer[index] = storage[address + index];
+    }
+    last_call_callback = callback;
+  }
+  void write(std::size_t address, std::span<const std::uint8_t> buffer, libmcu::NonBlocking* callback = nullptr) {
+    if (address > storage.size()) {
+      minunit_report("Storage_driver_mock: Write out of bounds\n");
+      return;
+    }
+    for (std::size_t index = 0; index < buffer.size(); index++) {
+      storage[address + index] = buffer[index];
+    }
+    last_call_callback = callback;
+  }
+  void call_callback(libmcu::Results result) {
+    if (last_call_callback != nullptr) {
+      last_call_callback->callback(result);
+    } else {
+      minunit_report("Storage_driver_mock: No callback stored\n");
+    }
+  }
+  constexpr std::size_t size() const noexcept {
+    return storage_size;
+  }
+
+  constexpr std::size_t page_size() const noexcept {
+    return storage_page_size;
+  }
+
+ private:
+  std::span<std::uint8_t> storage;
+  std::size_t storage_page_size;
+  std::size_t storage_size;
+  libmcu::NonBlocking* last_call_callback;
+};
+
+std::array<std::uint8_t, 128> storage_buffer;
+std::array<std::uint8_t, 16> settings_buffer;
+Storage_driver_mock storage_driver_dut(storage_buffer, 16);
+Settings_storage<Test_storage, storage_driver_dut> settings_storage_dut(settings_buffer, 0x32);
+
+MINUNIT_SETUP(settings_storage_setup) {
+  storage_buffer.fill(0xFF);
+  MINUNIT_PASS();
+}
+
+MINUNIT_TEARDOWN(settings_storage_teardown) {
+  settings_storage_dut.deinit();
+  MINUNIT_PASS();
+}
+
+// Test initialization
+MINUNIT_ADD(settings_storage_init_empty, settings_storage_setup, settings_storage_teardown) {
+  std::size_t timeout_count = 1000;
+  Test_storage test_storage{1, 2, 3};
+  Test_storage test_storage_defaults{4, 5, 6};
+  MINUNIT_CHECK(settings_storage_dut.get_state() == libmcu::States::Uninitialized);
+  settings_storage_dut.init(test_storage, test_storage_defaults);
+  MINUNIT_CHECK(settings_storage_dut.get_state() == libmcu::States::Initializing);
+  while (timeout_count > 0 && settings_storage_dut.get_state() != libmcu::States::Idle) {
+    settings_storage_dut.progress();
+    timeout_count--;
+    storage_driver_dut.call_callback(libmcu::Results::NoError);
+  }
+  // check if defaults settings are written to storage
+  MINUNIT_CHECK(test_storage.value_one == 4);
+  MINUNIT_CHECK(test_storage.value_two == 5);
+  MINUNIT_CHECK(test_storage.value_three == 6);
+  // check if default settings are written to address 0
+  MINUNIT_CHECK(storage_buffer[0] == 0x32);
+  MINUNIT_CHECK(storage_buffer[1] == 0x00);
+  MINUNIT_CHECK(storage_buffer[2] == 0x00);
+  MINUNIT_CHECK(storage_buffer[3] == 0x00);
+  MINUNIT_CHECK(storage_buffer[4] == 0x04);
+  MINUNIT_CHECK(storage_buffer[6] == 0x05);
+  MINUNIT_CHECK(storage_buffer[7] == 0x00);
+  MINUNIT_CHECK(storage_buffer[8] == 0x06);
+  MINUNIT_CHECK(storage_buffer[9] == 0x00);
+  MINUNIT_CHECK(storage_buffer[10] == 0x00);
+  MINUNIT_CHECK(storage_buffer[11] == 0x00);
+  MINUNIT_CHECK(storage_buffer[12] == 0x00);
+  MINUNIT_CHECK(storage_buffer[13] == 0x00);
+  MINUNIT_CHECK(storage_buffer[14] == 0x00);
+  MINUNIT_CHECK(storage_buffer[15] == 0x00);
+  MINUNIT_CHECK(settings_storage_dut.get_state() == libmcu::States::Idle);
+  MINUNIT_CHECK(timeout_count > 0);
+}
+
+// test init with already present settings
+MINUNIT_ADD(settings_storage_init_not_empty, settings_storage_setup, settings_storage_teardown) {
+  std::size_t timeout_count = 1000;
+  Test_storage test_storage{1, 2, 3};
+  Test_storage test_storage_defaults{4, 5, 6};
+  detail::Settings_storage_record<Test_storage>* record =
+    reinterpret_cast<detail::Settings_storage_record<Test_storage>*>(settings_buffer.data());
+  record->magic_version = 0x32;
+  record->sequence_number = 0;
+  record->checksum = 0;
+  record->settings.value_one = 7;
+  record->settings.value_two = 8;
+  record->settings.value_three = 9;
+  storage_driver_dut.write(0, settings_buffer);
+  MINUNIT_CHECK(settings_storage_dut.get_state() == libmcu::States::Uninitialized);
+  settings_storage_dut.init(test_storage, test_storage_defaults);
+  while (timeout_count > 0 && settings_storage_dut.get_state() != libmcu::States::Idle) {
+    settings_storage_dut.progress();
+    timeout_count--;
+    storage_driver_dut.call_callback(libmcu::Results::NoError);
+  }
+  // check if present settings are written to storage
+  MINUNIT_CHECK(test_storage.value_one == 7);
+  MINUNIT_CHECK(test_storage.value_two == 8);
+  MINUNIT_CHECK(test_storage.value_three == 9);
+}
+
+// test init with already present settings and wrapping sequence numbers
+// test init with some pages filled with different data
+// test init with some pages filled with different data and wrapping sequence numbers
+// test settings write
+// test settings write with wrapping sequence numbers
+// test settings read
